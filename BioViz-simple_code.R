9759d666bccde8a481f1df8e1ecc47fc0c9faba7
Peter Imonte <imontepez@gmail.com>
2026-02-06 18:11:18 +0100

    Update BioViz-simple_code.R

--- BioViz-simple_code.R
+++ BioViz-simple_code.R
@@ -14,6 +14,6 @@ reason2 <- "PCSK9 turned a complex, polygenic trait (heart disease) into somethi
 
 # final output ----
 
-my_essay <- paste0(" Hi, my name is ", my_name , " a graduate of Biomedical Science from ", affliation1, " currently interning as laboratory scientist at ", affliation2, " As a laboratory scientist, my most preffered gene is ", preffered_gene, " found in ", gene_orgm, " I preffer ",  preffered_gene,  " becacuse ", reason1,  " and ", reason2 )
+my_essay <- paste("Hi, my name is", my_name , "a graduate of Biomedical Science from", affliation1, "currently interning as laboratory scientist at", affliation2, "As a laboratory scientist, my most preffered gene is", preffered_gene, "found in", gene_orgm, "I preffer",  preffered_gene,  "becacuse", reason1,  "and", reason2 )
 
 my_essay
